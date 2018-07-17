bee5ca90c30f61aac8b014b658ddaeb0c69c60e2
MorganHuegel <mhue2910@gmail.com>
2018-07-17 14:07:24 -0400

    updated main.css to change edit-box .rating fstyling

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -19,18 +19,3 @@ api.getBookmarks ( response => {
 $(eventListeners.main);
 
 
-// store.bookmarks = dummyData;
-// bookmarkList.render();
-
-
-//api.addBookmark(dummyData[1]);
-//api.deleteBookmark('cjiopfl6s000n0kug349pk34c', () => console.log('item deleted'));
-
-//api.getBookmarks(() => console.log('got the package')));
-// store.bookmarks = dummyData;
-// store.updateBookmark('55556666', {title: 'Thinkful Homepage'});
-// api.updateBookmark('cjioq1eqo000s0kugpzja8rh3', {title: 'Something Else', desc:"Something Else"});
-// console.log(store.bookmarks);
-
-// store.bookmarks = dummyData;
-// bookmarkList.render();
